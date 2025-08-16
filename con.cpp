52ee00189b682bfcc0adf2b30fdee0581bfb2dcc
Rajdeep Jadhao <141089814+raj607-dev@users.noreply.github.com>
2025-08-16 17:43:43 +0530

    Create con.cpp

---
+++ con.cpp
@@ -0,0 +1,15 @@
+<Configuration>
+  <Add OfficeClientEdition="64" Channel="PerpetualVL2024">
+    <Product ID="ProPlus2024Volume" PIDKEY="#####-#####-#####-#####-#####">
+      <Language ID="en-us" />
+    </Product>
+    <Product ID="VisioPro2024Volume" PIDKEY="#####-#####-#####-#####-#####">
+      <Language ID="en-us" />
+    </Product>
+    <Product ID="ProjectPro2024Volume" PIDKEY="#####-#####-#####-#####-#####">
+      <Language ID="en-us" />
+    </Product>
+  </Add>
+  <RemoveMSI />
+  <Property Name="AUTOACTIVATE" Value="1" />
+</Configuration>
